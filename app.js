7e830c07a29d356c98630ba3ce6a32dfce2415ae
Marcus-V-Freitas <marcusviniciusdefreitascosta@yahoo.com.br>
2019-12-17 14:39:19 -0300

    inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".icone").on("mouseover", function(){
+		
+		$(".icone").css("opacity",0.3);
+		$(this).css("opacity",1);
+	});
+});
+
+$(document).ready(function(){
+    alert("Exemplo em Jquery");
+});
